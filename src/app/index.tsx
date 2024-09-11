43a636d4beb5bf6a7ac84e1da79623cfe4a2dc06
Hussain <jakirdeveloper@gmail.com>
2024-09-11 22:36:13 +0530

    base

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,7 +8,9 @@ import {
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-import particlesOptions  from './particles-options'
+import particlesOptions from './particles-options'
+import { Button } from "@/components/ui/button";
+import { LuSun } from "react-icons/lu";
 
 const App = () => {
   const [init, setInit] = useState(false);
@@ -32,18 +34,38 @@ const App = () => {
     console.log(container);
   };
 
-  const options: ISourceOptions = useMemo(
+  const options = useMemo(
     () => (particlesOptions),
     [],
   );
 
   if (init) {
     return (
-      <Particles
-        id="tsparticles"
-        particlesLoaded={particlesLoaded}
-        options={options}
-      />
+      <>
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options as unknown as ISourceOptions}
+        />
+        <div className="flex flex-col items-center justify-center w-full h-full gap-5 align-middle">
+          <div className="p-6">
+            <h1 className="text-5xl text-black">Jakir <span className="font-extrabold">Hussain</span></h1>
+          </div>
+          <div className="flex items-center justify-center w-full h-full gap-5 align-middle md:max-h-[800px] flex-col md:flex-row">
+            <div className="flex flex-col h-full gap-6">
+              <Button variant={'outline'}><LuSun /></Button>
+              <div className="flex flex-col gap-4 p-2 py-4 bg-white rounded-md">
+                <Button variant={'outline'}><LuSun /></Button>
+                <Button variant={'outline'}><LuSun /></Button>
+                <Button variant={'outline'}><LuSun /></Button></div>
+
+            </div>
+            <div className="h-full w-[300px] bg-white rounded-md p-4">Hello</div>
+            <div className="h-full w-[300px] bg-white rounded-md p-4">Hello</div>
+          </div>
+        </div>
+
+      </>
     );
   }
 
