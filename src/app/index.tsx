1dcae8776ba5ff18ad191a2500532da14c2feb2f
Hussain <jakirdeveloper@gmail.com>
2024-09-12 13:34:37 +0530

    fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,6 +11,7 @@ import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSl
 import particlesOptions from './particles-options'
 import { Button } from "@/components/ui/button";
 import { LuSun } from "react-icons/lu";
+import { Separator } from "@/components/ui/separator";
 
 const App = () => {
   const [init, setInit] = useState(false);
@@ -48,10 +49,10 @@ const App = () => {
           options={options as unknown as ISourceOptions}
         />
         <div className="flex flex-col items-center justify-center w-full h-full gap-5 align-middle">
-          <div className="p-6">
+          {/* <div className="p-6">
             <h1 className="text-5xl text-black">Jakir <span className="font-extrabold">Hussain</span></h1>
-          </div>
-          <div className="flex items-center justify-center w-full h-full gap-5 align-middle md:max-h-[800px] flex-col md:flex-row">
+          </div> */}
+          <div className="flex items-start justify-center w-full h-full gap-5 align-middle md:max-h-[800px] flex-col md:flex-row">
             <div className="flex flex-col h-full gap-6">
               <Button variant={'outline'}><LuSun /></Button>
               <div className="flex flex-col gap-4 p-2 py-4 bg-white rounded-md">
@@ -60,8 +61,33 @@ const App = () => {
                 <Button variant={'outline'}><LuSun /></Button></div>
 
             </div>
-            <div className="h-full w-[300px] bg-white rounded-md p-4">Hello</div>
-            <div className="h-full w-[300px] bg-white rounded-md p-4">Hello</div>
+            <div className="h-[620px] w-[360px] bg-white rounded-3xl rounded-tr-none p-10 flex flex-col justify-between align-middle items-center gap-6">
+              <img src='/avatar.webp' alt="avatar" className="border-2 border-black rounded-tr-none rounded-3xl" />
+              <div className="flex flex-col items-center justify-start gap-2 align-middle">
+                <p className="text-lg font-bold text-lime-800">Software Engineer</p>
+                <h1 className="text-3xl text-black">Jakir <span className="font-extrabold">Hussain</span></h1>
+              </div>
+              <div className="flex items-stretch justify-between gap-4">
+                <Button ><LuSun /></Button>
+                <Button ><LuSun /></Button>
+                <Button ><LuSun /></Button>
+                <Button ><LuSun /></Button>
+
+              </div>
+
+              <div className="flex flex-col w-full gap-4">
+                <Separator />
+
+                <div className="flex w-full gap-4">
+                  <Button variant={'outline'} >Download CV <LuSun /></Button>
+                  <Separator orientation="vertical" />
+
+                  <Button variant={'outline'}>Contact me <LuSun /></Button>
+                </div>
+              </div>
+
+            </div>
+            <div className="h-full w-[850px] bg-white rounded-3xl rounded-tl-none p-6">Hello</div>
           </div>
         </div>
 
