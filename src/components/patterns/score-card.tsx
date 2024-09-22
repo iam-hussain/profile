9cd95dde9ee12e689959e27a1d7c9671784867ad
Hussain <jakirdeveloper@gmail.com>
2024-09-22 23:49:47 +0530

    fix

--- src/components/patterns/score-card.tsx
+++ src/components/patterns/score-card.tsx
@@ -2,7 +2,7 @@
 const ScoreCard = ({ hed, dek }: { hed: string, dek: string }) => {
     return (
         <div className="flex items-center justify-center gap-2 py-4 align-middle">
-            <span className="text-5xl">{hed}</span>
+            <span className="text-3xl lg:text-5xl">{hed}</span>
             <p className="text-sm text-foreground/80 max-w-[100px]">{dek}</p>
         </div>
     )
