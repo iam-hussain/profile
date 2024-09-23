6b8e4b68eea92c9271522ab228c31fc128daaf92
Hussain <jakirdeveloper@gmail.com>
2024-09-24 01:28:40 +0530

    fix

--- src/components/compositions/skills.tsx
+++ src/components/compositions/skills.tsx
@@ -6,15 +6,15 @@ import SkillsWordCloud from '../patterns/skills-word-cloud';
 const Skills = () => {
 
     return (
-        <div className="flex flex-col 4xl:gap-12 gap-8 2xl:px-6 py-8 4xl:py-12 w-full justify-center align-middle items-center" id="skill">
+        <div className="flex flex-col items-center justify-center w-full gap-6 py-8 align-middle 2xl:px-6 4xl:py-12" id="skill">
             <div className="flex flex-col items-start w-full gap-4">
                 <div className='flex items-center justify-center w-auto gap-2 px-2 py-1 align-middle border rounded-md'>
                     <Dumbbell />
                     <span className="text-sm font-medium uppercase text-foreground/60">Skills</span>
                 </div>
-                <h2 className="4xl:text-3xl text-2xl font-bold">Technology Stack</h2>
+                <h2 className="text-2xl font-bold 4xl:text-3xl">Technology Stack</h2>
             </div>
-            <div className="flex max-w-xl w-full">
+            <div className="flex w-full max-w-4xl">
                 <SkillsWordCloud />
             </div>
 
